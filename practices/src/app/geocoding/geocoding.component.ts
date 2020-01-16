b946ea059c056b17e6ee14c535f141a215f04743
Piotr Pyśk <defcon1994@gmail.com>
2020-01-16 22:20:51 +0100

    Reverse geocoding

--- practices/src/app/geocoding/geocoding.component.ts
+++ practices/src/app/geocoding/geocoding.component.ts
@@ -12,13 +12,27 @@ export class GeocodingComponent {
   results$: Promise<google.maps.GeocoderResult[]>;
   errorMessage?: string;
 
-  constructor(private geocoder: GeocoderService) { }
+  lat: number;
+  lng: number;
+  reverseGeocodeResults$: Promise<google.maps.GeocoderResult[]>;
+  reverseGeocodeErrorMessage?: string;
+
+  constructor(private readonly geocoder: GeocoderService) { }
 
   onSubmit(): void {
     delete this.errorMessage;
     this.results$ = this.geocoder.geocode({ address: this.address })
       .catch((status: google.maps.GeocoderStatus) => {
         this.errorMessage = `Otrzymano następujący status: ${status}`;
+        return [];
+      });
+  }
+
+  onReverseGeocodeSubmit(): void {
+    delete this.reverseGeocodeErrorMessage;
+    this.reverseGeocodeResults$ = this.geocoder.geocode({location: {lat: this.lat, lng: this.lng}})
+      .catch((status: google.maps.GeocoderStatus) => {
+        this.reverseGeocodeErrorMessage = `Otrzymano następujący status: ${status}`;
         return [];
       });
   }
